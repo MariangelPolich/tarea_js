1fa3c6e1e0762bf8617ae0b605558f8e810d5a7d
Mariangel Polich <legnairamaegis@hotmail.com>
2021-04-26 15:59:00 -0300

    js tarea 2 funcional

---
+++ javascript 2/index.js
@@ -0,0 +1,4 @@
+const primero = prompt('ponga el primer valor');
+const segundo = prompt('ponga el segundo valor');
+const resultado = ( +primero + +segundo );
+alert('su resultado es: '+resultado);
