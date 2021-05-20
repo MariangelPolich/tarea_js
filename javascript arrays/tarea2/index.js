c283af4afaab396ae67ce942f8d77d4cd1cf1052
Mariangel Polich <legnairamaegis@hotmail.com>
2021-05-20 13:11:23 -0300

    Tarea 2 funcional

---
+++ javascript arrays/tarea2/index.js
@@ -0,0 +1,28 @@
+let nota =null;
+
+while (nota == null) {
+    nota = prompt('Ingrese un número del 0 al 10');
+    if (nota < 0 || nota > 10) {
+        nota = null;
+        alert('Número inválido');
+    }
+}
+
+if (nota == 0 || nota == 1 || nota == 2) {
+    alert('Muy deficiente');
+}
+if (nota == 3 || nota == 4) {
+    alert('Insuficiente');
+}
+if (nota == 5 || nota == 6) {
+    alert('Suficiente');
+}
+if (nota == 7) {
+    alert('Bien');
+}
+if (nota == 8 || nota == 9) {
+    alert('Notable');
+}
+if (nota == 10) {
+    alert('Sobresaliente');
+}
