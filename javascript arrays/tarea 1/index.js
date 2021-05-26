342f3a234a92005db7dde95f7c4149f77921e8f0
Mariangel Polich <legnairamaegis@hotmail.com>
2021-05-26 16:18:38 -0300

    Tarea corregida

--- javascript arrays/tarea 1/index.js
+++ javascript arrays/tarea 1/index.js
@@ -1,9 +1,9 @@
 const edad = prompt('ponga su edad prro');
 let texto = 'usted es';
-if (edad >= 18) {
-    texto = texto + ' adulto. Puede conducir e ir preso, felicidades.'
+if (Number.isNaN(+edad)) {
+    alert("Ingrese un número válido");
+} else if (edad >= 18) {
+    texto = texto + ' adulto. Puede conducir e ir preso, felicidades.';
+    document.write (texto)
 }
-if (edad < 18) {
-    texto = texto + ' menor. Vaya a tomar el bondi'
-}
-document.write (texto)
+
---
+++ javascript arrays/tarea 3/index.js
@@ -0,0 +1,8 @@
+let x = true;
+while (x == true) {
+    prompt('Escriba palabritas');
+    document.write(x + "- ");
+    if (x == null) {
+    alert('listo!')    
+    }
+}
