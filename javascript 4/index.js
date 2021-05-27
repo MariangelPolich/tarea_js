ea8f3c7a6e5edaf974d3ea693ec1c4d750eae23b
Mariangel Polich <legnairamaegis@hotmail.com>
2021-05-27 18:06:19 -0300

    ejercicio 4 corregido

--- javascript 4/index.js
+++ javascript 4/index.js
@@ -1,4 +1,14 @@
-const primer_texto = prompt('ingrese el primer texto');
-const segundo_texto = prompt('ingrese el segundo texto');
-const resultado = ( primer_texto + " " + segundo_texto );
-console.log('su resultado es: '+resultado);
+let suma = 0
+let num = prompt("ponga números")
+
+while (num !== null) {
+    if (Number.isNaN(+num)) {
+        alert("no e un número eso");
+        num = prompt("ponga otro número");
+    } 
+    else {
+        suma = suma + +num;
+        num = prompt("ponga otro número");
+    }
+}
+document.write(suma);
