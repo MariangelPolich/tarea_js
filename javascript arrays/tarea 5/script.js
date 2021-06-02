2f5665988eb53e4881069e4e890356b7e648ba19
Mariangel Polich <legnairamaegis@hotmail.com>
2021-06-02 19:40:37 -0300

    tarea 5 funcional

---
+++ javascript arrays/tarea 5/script.js
@@ -0,0 +1,16 @@
+/*Realizar una página con un script que calcule el valor de la letra de un número de DNI (Documento nacional de identidad).
+El algoritmo para calcular la letra del dni es el siguiente :
+El número debe ser entre 0 y 99999999
+Debemos calcular el resto de la división entera entre el número y el número 23.
+Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
+Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
+Deberá de repetirse el proceso hasta que el usuario pulse «cancelar»*/
+
+function conseguir_letra(dni) {
+    const resto = dni % 23;
+    const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    return abc[resto]
+}
+const documento = prompt("ponga el dni");
+const letra = conseguir_letra(+documento);
+console.log(letra)
