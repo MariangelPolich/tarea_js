9ac183705942743da36f6a20e9aefb052124b5cd
Mariangel Polich <legnairamaegis@hotmail.com>
2021-07-14 21:58:02 -0300

    Mensaje prueba

--- javascript arrays/tarea 5/script.js
+++ javascript arrays/tarea 5/script.js
@@ -4,7 +4,7 @@ El número debe ser entre 0 y 99999999
 Debemos calcular el resto de la división entera entre el número y el número 23.
 Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
 Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
-Deberá de repetirse el proceso hasta que el usuario pulse «cancelar»*/
+Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».*/
 
 function conseguir_letra(dni) {
     const resto = dni % 23;
